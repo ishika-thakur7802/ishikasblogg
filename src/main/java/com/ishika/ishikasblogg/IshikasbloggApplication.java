b495b4f9bb43beac9b1bafac07739b4ad9eda3b6
Ishika Thakur <ishikathakur0708@gmail.com>
2025-05-26 22:13:01 +0530

    Initial commit

---
+++ src/main/java/com/ishika/ishikasblogg/IshikasbloggApplication.java
@@ -0,0 +1,13 @@
+package com.ishika.ishikasblogg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IshikasbloggApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IshikasbloggApplication.class, args);
+	}
+
+}
