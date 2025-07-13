d4069e46c37e83dd5865c869debd36c41135bb5d
Ishika Thakur <ishikathakur0708@gmail.com>
2025-07-13 12:26:38 +0530

    adding manual getters and setters cause lombok isnt working somehow

--- src/main/java/com/ishika/ishikasblogg/entities/Blog.java
+++ src/main/java/com/ishika/ishikasblogg/entities/Blog.java
@@ -5,10 +5,8 @@ import lombok.*;
 
 
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
+@ToString
+
 public class Blog {
     @Id @GeneratedValue
     private Long id;
@@ -17,10 +15,67 @@ public class Blog {
     @Column(columnDefinition = "TEXT")
     private String content;
     private String imageUrl;
+    private int likes;
+
+
+
+    public Blog() {
+        // no-arg constructor for JPA & Jackson
+    }
+
+    public Blog(Long id, String title, String content, String imageUrl, int likes) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.imageUrl = imageUrl;
+        this.likes = likes;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setId(long id)
+    {
+        this.id= id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
 //
 //    @ManyToOne
 //    private User author;
 
-    private int likes;
+
 }
 
